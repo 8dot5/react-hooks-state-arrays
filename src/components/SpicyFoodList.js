cd5885f8598eb576f90b6534773ddbcb9c8e9cce
Chun Yi <8.5by11@gmail.com>
2022-02-02 20:45:33 -0500

    commit tree a095fadfae201383106fd89bcd67e5ede0d4ad71

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,14 +6,37 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    // console.log(newFood);
+
+    // make a copy of our foods array, and insert each element into a new array. We're also adding the newly generated food returned by the getNewSpicyFood function at the end of the array.
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray)
   }
 
+  // .map method: updates items in array by creating a new array.
+  // here its applying modifications to the food.heatLevel
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+  
+  // .map method: updates items in array by creating a new array.
+  // here its grabbing the food.id which will be used to update the food.heatLevel
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
 
   return (
     <div>
